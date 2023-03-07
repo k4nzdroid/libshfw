4bc3a977398c6ca49d96bce660d43aa751cf0c1a
wangpengfei <wangpengfei@rivtower.com>
2023-03-07 15:47:47 +0800

    fix: reanme util

---
+++ util/date.lib.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+_date_as_filename() {
+    echo $(date +%Y%m%d%H%M%S)
+}
--- util/string.lib.sh
+++ util/string.lib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-str_ends_with() {
+_str_ends_with() {
     echo "$1" | grep -q -E "$2"'$' && echo true || echo false
 }
